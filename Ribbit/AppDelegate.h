bee61ea2fa1928d84cdf853781e7c49546e6d74e
Alex Knows <cruzbiz11@gmail.com>
2015-09-24 13:54:40 -0400

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Alex Cruz on 9/24/15.
+//  Copyright © 2015 Alex Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Ribbit
+//
+//  Created by Alex Cruz on 9/24/15.
+//  Copyright © 2015 Alex Cruz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
